955baa73bbc1181cb2881f72222e7016f0a01fc8
shortercode <iainshorter@Iains-MacBook-Pro.local>
2016-11-03 09:05:58 +0000

    pulse implementation
    destubbed PulseLogicBlock, uses a rising edge logic with a decrementing
    counter

--- src/logicComponents/logic/Pulse.js
+++ src/logicComponents/logic/Pulse.js
@@ -1,11 +1,21 @@
 class PulseLogicBlock extends LogicBlock {
-	constructor (duration = 1, strength = MAX_SIGNAL) {
-		super();
-		this.duration = duration;
-		this.counter = 0;
-		this.lastInputValue = 0;
-	}
-	tick () {
-		var value = this.input ? this.input.value : 0;
-	}
+  constructor(duration = 1) {
+    super();
+    this.duration = 1;
+    this.counter = 0;
+    this.value = 0;
+    this.lastValue = 0;
+  }
+  tick() {
+    let value = this.input ? this.input.value : 0;
+
+    this.counter = this.counter == 0 ? 0 : this.counter - 1;
+    
+    if (value > this.lastValue) {
+      this.counter = this.duration;
+    }
+
+    this.value = this.counter > 0 ? MAX_SIGNAL : 0;
+    this.lastValue = value;
+  }
 }
