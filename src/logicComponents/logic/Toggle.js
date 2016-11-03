8fb37b221c2bcd2294933da736d04b093561a62b
shortercode <iainshorter@Iains-MacBook-Pro.local>
2016-11-03 09:11:06 +0000

    toggle implementation
    removed lock free update logic
    component now treated as “instant”
    tick method using rising edge logic to trigger the “toggle”

--- src/logicComponents/logic/Toggle.js
+++ src/logicComponents/logic/Toggle.js
@@ -1,20 +1,15 @@
 class ToggleLogicBlock extends LogicBlock {
-	constructor (power = MAX_SIGNAL, active) {
-		super();
-		this.power = power;
-		this.value = active ? power : 0;
-	}
-	tick () {
-		// do nothing
-	}
-	update () {
-		// ToggleLogicBlock does not have a locked state
-		if (this.readState === STATE_DONE)
-			return;
-		this.readState = STATE_DONE;
-		if (this.input) {
-			this.input.update();
-			this.tick();
-		}
-	}
+  constructor(power = MAX_SIGNAL, active) {
+    super();
+    this.power = power;
+    this.value = active ? power : 0;
+    this.lastValue = 0;
+  }
+  tick() {
+    let value = this.input ? this.input.value : 0;
+    if (value > this.lastValue) {
+      this.value = this.value ? 0 : this.power;
+    }
+    this.lastValue = value;
+  }
 }
