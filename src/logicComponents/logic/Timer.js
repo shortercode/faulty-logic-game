c8a747d13ef037d5a7b5d49bca29b4d8d5388c92
shortercode <iainshorter@Iains-MacBook-Pro.local>
2016-11-03 09:21:45 +0000

    timer implementation
    uses decrementing counter
    that triggers and resets when the value hits 0
    also resets but doesn’t output when receiving an input

--- src/logicComponents/logic/Timer.js
+++ src/logicComponents/logic/Timer.js
@@ -1,3 +1,16 @@
 class TimerLogicBlock extends LogicBlock {
-	
+  constructor(interval = 10) {
+    super();
+    this.interval = interval;
+    this.counter = interval;
+  }
+  tick() {
+    let input = this.input && this.input.value;
+    this.counter = input ? this.interval : this.counter - 1;
+    this.value = 0;
+    if (this.counter <= 0) {
+      this.value = MAX_SIGNAL;
+      this.counter = this.interval;
+    }
+  }
 }
